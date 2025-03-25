55baf2f0cbf245e8ffe0e55805951cc1bea57c93
Caio Lessa <caio.lessa01@fatec.sp.gov.br>
2025-03-24 22:22:39 -0300

    Sistema SmartTv

--- SmartTV/src/SmartTv.java
+++ SmartTV/src/SmartTv.java
@@ -1,3 +1,32 @@
 public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
 
+    public void ligar(){
+        this.ligada = true;
+    }
+    public void desligar(){
+        this.ligada = false;
+    }
+    public void aumentarVolume(){
+        this.volume++;
+        System.out.println("Aumentando volume da TV para: " + volume);
+    }
+    public void diminuirVolume(){
+        this.volume--;
+        System.out.println("Diminuindo volume da TV para: " + volume);
+    }
+    public void aumentarCanal(){
+        this.canal++;
+        System.out.println("Aumentando canal da TV para: " + canal);
+    }
+    public void diminuirCanal(){
+        this.canal--;
+        System.out.println("Diminuindo canal da TV para: " + canal);
+    }
+    public void mudarCanal(int canal){
+        this.canal = canal;
+        System.out.println("Mudando canal da TV para: " + canal);
+    }
 }
